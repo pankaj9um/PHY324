f15f8c7e2f0c1756dbbfb644cd001e3c698ae57d
Pankaj Patil <Pankaj.Patil@autodesk.com>
2022-03-09 22:44:00 -0500

    update

---
+++ Pendulum Project/Part 2/code/statslab.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy.optimize as optim
+import matplotlib.pyplot as plt
+
+# use bigger font size for plots
+plt.rcParams.update({'font.size': 20})
+
+def chi2(y_measure,y_predict,errors):
+    """Calculate the chi squared value given a measurement with errors and 
+    prediction"""
+    return np.sum( np.power(y_measure - y_predict, 2) / np.power(errors, 2) )
+
+def chi2reduced(y_measure, y_predict, errors, number_of_parameters):
+    """Calculate the reduced chi squared value given a measurement with errors
+    and prediction, and knowing the number of parameters in the model."""
+    return chi2(y_measure, y_predict, errors)/ \
+            (y_measure.size - number_of_parameters)
+
+def read_data(filename, skiprows=1, usecols=(0,1), delimiter=","):
+    """Load give\n file as csv with given parameters, 
+    returns the unpacked values"""
+    return np.loadtxt(filename,
+                      skiprows=skiprows, usecols=usecols, 
+                      delimiter=delimiter,
+                      unpack=True)
+
+def fit_data(model_func, xdata, ydata, yerrors, guess=None):
+    """Utility function to call curve_fit given x and y data with errors"""
+    popt, pcov = optim.curve_fit(model_func, 
+                                xdata, ydata, absolute_sigma=True, 
+                                sigma=yerrors,
+                                p0=guess)
+
+    pstd = np.sqrt(np.diag(pcov))
+    return popt, pstd
+
